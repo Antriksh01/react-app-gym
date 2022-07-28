62660ace60251b9e5c1639a4e97845ac6cfbf890
Mahesh Kuldeep <mkuldeep313@gmail.com>
2022-07-28 20:59:18 +0530

    Updates

---
+++ precache-manifest.cb3e0c62292b2ea3c014cb1bd7432a8c.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ed21e4eaa2345c9dca6fa9442a778bc",
+    "url": "/react-gym-website/index.html"
+  },
+  {
+    "revision": "8d21690e32bfd9d72f02",
+    "url": "/react-gym-website/static/css/main.c88eb0c2.chunk.css"
+  },
+  {
+    "revision": "e61042ad6711758af6d2",
+    "url": "/react-gym-website/static/js/2.d818b3c0.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react-gym-website/static/js/2.d818b3c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d21690e32bfd9d72f02",
+    "url": "/react-gym-website/static/js/main.11d961bb.chunk.js"
+  },
+  {
+    "revision": "bcd7b824e58282ed50f3",
+    "url": "/react-gym-website/static/js/runtime-main.0d42a81d.js"
+  },
+  {
+    "revision": "d949e940dde9139a94481399ed0aa298",
+    "url": "/react-gym-website/static/media/HeroImage1.d949e940.png"
+  },
+  {
+    "revision": "fe8b35e79196b141d344afe1007aa859",
+    "url": "/react-gym-website/static/media/JoinUs2.fe8b35e7.png"
+  },
+  {
+    "revision": "83e019f14222ea38febf9ee965e40916",
+    "url": "/react-gym-website/static/media/features1.83e019f1.svg"
+  },
+  {
+    "revision": "9b99e1877ea384fe9de27fe784da4afe",
+    "url": "/react-gym-website/static/media/features2.9b99e187.svg"
+  },
+  {
+    "revision": "f93ab23617fd630eae3f7f70871ccd90",
+    "url": "/react-gym-website/static/media/features3.f93ab236.svg"
+  },
+  {
+    "revision": "a609d5e7c4149ca1e4c342521453d3c1",
+    "url": "/react-gym-website/static/media/features4.a609d5e7.svg"
+  },
+  {
+    "revision": "11dc41bbad34d3d91a0d938afb799f92",
+    "url": "/react-gym-website/static/media/gallery.11dc41bb.png"
+  },
+  {
+    "revision": "7ee3bdc1078c458006ca81e11f7b3c23",
+    "url": "/react-gym-website/static/media/gallery1.7ee3bdc1.png"
+  },
+  {
+    "revision": "dabd4b71bac68e3f5ab58fb7353fd5f7",
+    "url": "/react-gym-website/static/media/gallery2.dabd4b71.png"
+  },
+  {
+    "revision": "688011ae6a83edace610f1116f8bba17",
+    "url": "/react-gym-website/static/media/gallery3.688011ae.png"
+  },
+  {
+    "revision": "ee2b9bbbc3be9962a497f6fbd3f14456",
+    "url": "/react-gym-website/static/media/gallery4.ee2b9bbb.png"
+  },
+  {
+    "revision": "27bfe8863f3d03f63659ff2faf57379b",
+    "url": "/react-gym-website/static/media/gallery5.27bfe886.png"
+  },
+  {
+    "revision": "4a86a33c03dda10cd9e2683520ab3349",
+    "url": "/react-gym-website/static/media/girlbox.4a86a33c.jpg"
+  },
+  {
+    "revision": "1872b83d4bb613d7eb3a4ba8a7a0aa6a",
+    "url": "/react-gym-website/static/media/shivigymhero.1872b83d.jpg"
+  },
+  {
+    "revision": "b969262b5e88ccd75604b418b6f89fa1",
+    "url": "/react-gym-website/static/media/summerBg.b969262b.webp"
+  },
+  {
+    "revision": "2649ade76b865eab3ebf88315cf26e37",
+    "url": "/react-gym-website/static/media/trainer.2649ade7.png"
+  },
+  {
+    "revision": "0f8699515744095b05bff662ac0c95ac",
+    "url": "/react-gym-website/static/media/trainer2.0f869951.png"
+  },
+  {
+    "revision": "69074cb06197ce9e7e8f9e95c5064222",
+    "url": "/react-gym-website/static/media/trainer3.69074cb0.png"
+  },
+  {
+    "revision": "5c1449823a877fd6de3d320e42556412",
+    "url": "/react-gym-website/static/media/tyrepush1.5c144982.jpg"
+  }
+]);
